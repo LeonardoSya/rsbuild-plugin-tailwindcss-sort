db0d5c1c0b19675b9de8ca5a80a45efdcd11e1fe
LeonardoSya <2246866774@qq.com>
2025-04-03 11:08:24 +0800

    feat: before test

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import type { RsbuildPlugin } from "@rsbuild/core";
+import { transformTailwindcssClassOrder } from "./transform.ts";
+
+export const RsbuildPluginTailwindcssSort = (): RsbuildPlugin => ({
+  name: "rsbuild-plugin-tailwindcss-sort",
+
+  // setup作为插件逻辑的主入口  api对象包含了各类hook和工具函数
+  setup(api) {
+    api.transform({ test: /\.(jsx|tsx)$/ }, ({ code }) =>
+      transformTailwindcssClassOrder(code)
+    );
+
+    // 在开发构建完成时
+    api.onDevCompileDone(({ isFirstCompile }) => {
+      if (isFirstCompile)
+        console.log(
+          "🎨 Tailwind CSS 类名已被 rsbuild-plugin-tailwindcss-sort 排序"
+        );
+    });
+
+    // 在生产构建完成时
+    api.onAfterBuild(() => {
+      console.log(
+        "🎨 Tailwind CSS 类名已被 rsbuild-plugin-tailwindcss-sort 排序"
+      );
+    });
+  },
+});
+
+export default RsbuildPluginTailwindcssSort;
--- src/transform.ts
+++ src/transform.ts
@@ -1,6 +1,40 @@
+import * as parser from "@babel/parser"; // 代码解析为ast
+import traverse from "@babel/traverse"; // 遍历ast
+import generate from "@babel/generator"; // 将ast转回代码
+import * as t from "@babel/types"; // 用于ast节点的类型检查和创建
+
 const sortTailwindcssClasses = (classStr: string): string => {
   // 样式按字母顺序排序
-  return classStr.trim().split(/\s+/).sort().join(' ')
-}
+  return classStr.trim().split(/\s+/).sort().join(" ");
+};
 
+export const transformTailwindcssClassOrder = (code: string): string => {
+  // 将源码解析为ast
+  const ast = parser.parse(code, {
+    sourceType: "module", // 按es模块解析
+    plugins: ["jsx", "typescript"], // 支持jsx和ts语法
+  });
 
+  // 遍历ast
+  traverse(ast, {
+    // 当遇到jsx时执行此函数
+    JSXAttribute(path: { node: any }) {
+      console.log(path);
+      const { node } = path;
+      // 检查是否是className属性且值为字符串字面量
+      if (
+        t.isJSXIdentifier(node.name, { name: "className" }) &&
+        t.isStringLiteral(node.value)
+      ) {
+        const original = node.value.value;
+        const sorted = sortTailwindcssClasses(original);
+        if (original !== sorted) {
+          node.value = t.stringLiteral(sorted);
+        }
+      }
+    },
+  });
+
+  // 修改后的ast转回代码字符串
+  return generate(ast).code;
+};
